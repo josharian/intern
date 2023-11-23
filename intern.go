6ac414e9f3d82932be909e6cc581123c7953e6bc
Fuchun Zhang <ahfuzhang@gmail.com>
2023-11-23 17:02:47 +0800

    add Equal func

--- intern.go
+++ intern.go
@@ -6,7 +6,10 @@
 // with themselves and each other.
 package intern
 
-import "sync"
+import (
+	"sync"
+	"unsafe"
+)
 
 var (
 	pool sync.Pool = sync.Pool{
@@ -41,4 +44,17 @@ func Bytes(b []byte) string {
 	m[s] = s
 	pool.Put(m)
 	return s
+}
+
+// StringHeader copy from $(go env GOROOT)/src/reflect/value.go
+type StringHeader struct {
+	Data uintptr
+	Len  int
+}
+
+// Equal compare two strings. If s1 and s2 are both interned, it's only 0.9 ns/op
+func Equal(s1 string, s2 string) bool {
+	x1 := (*StringHeader)(unsafe.Pointer(&s1))
+	x2 := (*StringHeader)(unsafe.Pointer(&s2))
+	return x1.Len == x2.Len && x1.Data == x2.Data || s1 == s2
 }
